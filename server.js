d52ffc7bb72bc4319b743d8f2f1dcf8ad7227b9a
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-02-25 20:38:06 -0800

    finish yelp

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const PARKS_API_KEY = process.env.PARKS_API_KEY;
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
-const RESTAURANT_API_KEY = process.env.RESTAURANT_API_KEY;
+const YELP_API_KEY = process.env.YELP_API_KEY;
 // ============== Routes ================================
 app.get('/location', handleGetLocation);
 app.get('/weather', handleWeatherRequest);
@@ -119,38 +119,39 @@ function Parks(parksData) {
 }
 
 function handleGetRestaurants(req, res) {
-  const city = req.query.city;
-  // const url5 = `https://developer.nps.gov/api/v1/parks?q=${city}&api_key=${RESTAURANT_API_KEY}`;
-  // superagent.get(url5)
-  //   .then(stuffThatComesBack4 => {
-  //     const restaurantsArray = stuffThatComesBack4.body.data.map(restaurant => new Parks(restaurant))
-  res.send([
-    {
-      "name": "Pike Place Chowder",
-      "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
-      "price": "$$   ",
-      "rating": "4.5",
-      "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
-    }
-  ]);
-
-  //     restaurantsArray);
-  // })
-  // .catch(errorThatComesBack => {
-  //   console.log(errorThatComesBack);
-  //   res.status(500).send('Sorry something went wrong with the RESTAURANTS');
-  // });
+  const offset = (req.query.page - 1) * 5;
+  const url7 = `https://api.yelp.com/v3/businesses/search?term=restaurant&limit=5&latitude=${req.query.latitude}&longitude=${req.query.longitude}&offset=${offset}`;
+  superagent.get(url7)
+    .set('authorization', `bearer ${YELP_API_KEY}`)
+    .then((output) => {
+      console.log('here is THE OUTPUT', output.body.businesses);
+      const yelpArray = output.body.businesses.map((singleRestaurant) => new Restaurant(singleRestaurant));
+      res.send(yelpArray);
+    })
+    .catch(errorThatComesBack => {
+      console.log(errorThatComesBack);
+      res.status(500).send('Sorry something went wrong with them Restaurants !!!!');
+    });
 }
 
+function Restaurant(restaurantData) {
+  this.name = restaurantData.name;
+  this.image_url = restaurantData.image_url;
+  this.price = restaurantData.price;
+  this.rating = restaurantData.rating;
+  this.url = restaurantData.url;
+
+}
+
+
+
 function handleMovieRequests(req, res) {
-  console.log(req.query);
+  console.log('here is this HUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUGHE REQUEST OBJECT', req);
   const city = req.query.search_query;
   let url6 = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${city}&page=1&include_adult=false`;
   // console.log(url6);
   superagent.get(url6)
     .then(moviesResponse => {
-      // console.log('MOVIEEEEEEEEEEEEEEEEEEEEEEEES', { Movie: moviesResponse.body });
-      // const moviesArray = moviesResponse.body.results.map(movie => new Movie(movie));
       const moviesArray = moviesResponse.body.results.map(movie => new Movie(movie));
       res.send(moviesArray);
     })
